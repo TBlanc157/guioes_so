b9a0fcfb3afc5717884341a08e4e75144ffc401f
TBlanc157 <gmpinto11@gmail.com>
2021-05-10 17:08:18 +0100

    g4 ex1

---
+++ guiao4/ex1.c
@@ -0,0 +1,37 @@
+
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    int fout = open("saida.txt", O_CREAT || O_WRONLY || O_TRUNC, 0666);
+    int ferr = open("erros.txt", O_CREAT || O_WRONLY || O_TRUNC, 0666);
+    int fin  = open("/etc/passwd", O_RDONLY, NULL);
+    int res, i = 0;
+
+    res = dup2(fin, 0);
+    res = dup2(fout, 1);
+    res = dup2(ferr, 2);
+    close(fin);
+    close(fout);
+    close(ferr);
+
+    char buffer;
+    char line[128];
+    ssize_t read_res;
+    int end = 0;
+    while ((read_res = read(0, &buffer, 1)) > 0 && !end) {
+        line[i] = buffer;
+        i++;
+        if (buffer == '\n') {
+            write(1, line, i);
+            write(2, line, i);
+            fflush(stdout);
+            i = 0;
+        }
+    }
+    return 0;
+}
